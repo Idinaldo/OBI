480f4661b9b0b2667cfde1e53f383883c320e3d5
Idinaldo Oliveira <122240078+Idinaldo@users.noreply.github.com>
2024-02-17 13:20:38 -0300

    Update

---
+++ n2017/Fase 1/botas.c
@@ -0,0 +1,53 @@
+#include <cs50.h>
+#include <stdio.h>
+
+typedef struct
+{
+    int tamanho;
+    char lado;
+} DTBotas;
+
+
+int calculaPares(DTBotas array[], int len);
+
+int main(void)
+{
+
+    int qtd_botas = get_int("");
+    DTbotas botas[qtd_botas+10];
+
+    for (int i = 0; i < qtd_botas; i++)
+    {
+        int tamanho_bota;
+        char space, lado_bota;
+        scanf("%d%c%c", tamanho_bota, space, lado_bota);
+
+        botas[i].tamanho = tamanho_bota;
+        botas[i].lado = lado_bota;
+    }
+
+    int pares = calculaPares(botas, qtd_botas);
+    printf("%i\n", pares);
+}
+
+int calculaPares(DTBotas array[], int len)
+{
+
+    int qtd_pares;
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len; j++)
+        {
+            if (array[i].tamanho == array[j].tamanho)
+            {
+                if (array[i].lado != array[j].lado)
+                {
+                    qtd_pares++;
+                    break;
+                }
+            }
+        }
+    }
+
+    return qtd_pares;
+}
